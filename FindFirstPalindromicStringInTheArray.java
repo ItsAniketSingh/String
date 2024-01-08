e4722597ff8661ebf45f7935907958092a98e130
Aniket Singh <anikets101@gmail.com>
2024-01-08 11:52:03 +0530

    Strings Problem

---
+++ FindFirstPalindromicStringInTheArray.java
@@ -0,0 +1,56 @@
+/*
+    2108
+    Given an array of strings words, return the first palindromic string in the array. 
+    If there is no such string, return an empty string "".
+
+    A string is palindromic if it reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+Example 2:
+
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+
+ */
+
+
+
+public class FindFirstPalindromicStringInTheArray {
+    static class Solution {
+        public boolean isPalindrom(String s){
+            int i =0;
+            int n = s.length()-1;
+            while(i<n){
+                if(s.charAt(i) != s.charAt(n)){
+                    return false;
+                }
+                i++;
+                n--;
+            }
+            return true;
+        }
+        public String firstPalindrome(String[] words) {
+    
+            for(String s : words){
+                if(isPalindrom(s)){
+                    return s;
+                }
+            }
+            return "";
+            
+        }
+    }
+    public static void main(String[] args) {
+        String[]  words = {"notapalindrome","racecar"};
+
+       
+    }
+}
---
+++ ReverseVowelsOfAString.java
@@ -0,0 +1,59 @@
+/*
+    345
+    Given a string s, reverse only all the vowels in the string and return it.
+
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+Output: "holle"
+Example 2:
+
+Input: s = "leetcode"
+Output: "leotcede"
+
+
+
+*/
+
+
+
+
+public class ReverseVowelsOfAString {
+    class Solution {
+        public String reverseVowels(String s) {
+            int i = 0; 
+            int n = s.length()-1;
+            char temp;
+            char [] ch = s.toCharArray();
+            while(i<n){
+                if( (ch[i] == 'a' || ch[i] == 'A' || ch[i] == 'e' || ch[i] == 'E' || ch[i] == 'i' || ch[i] == 'I' || ch[i] == 'o' || ch[i] == 'O' || ch[i] == 'u' || ch[i] == 'U') && (ch[n] == 'a'|| ch[n] == 'A' || ch[n] == 'e' || ch[n] == 'E' || ch[n] == 'i' || ch[n] == 'I' || ch[n] == 'o' || ch[n] == 'O' || ch[n] == 'u' || ch[n] == 'U')){
+                    temp = ch[i];
+                    ch[i] = ch[n];
+                    ch[n] = temp;
+                    i++;
+                    n--;
+                }
+            else if(!(ch[i] == 'a' || ch[i] == 'A' || ch[i] == 'e' || ch[i] == 'E' || ch[i] == 'i' || ch[i] == 'I' || ch[i] == 'o' || ch[i] == 'O' || ch[i] == 'u' || ch[i] == 'U')){
+                n--;
+            }
+            else if(!(ch[n] == 'a'|| ch[n] == 'A' || ch[n] == 'e' || ch[n] == 'E' || ch[n] == 'i' || ch[n] == 'I' || ch[n] == 'o' || ch[n] == 'O' || ch[n] == 'u' || ch[n] == 'U')){
+                i++;
+            }
+    
+    
+            }
+            String result = new String(ch);
+            return result;
+    
+    
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
