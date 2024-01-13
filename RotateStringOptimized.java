8994ed99ea34e00e54f8c8bbe0f4dfa7752f5b8e
Aniket Singh <anikets101@gmail.com>
2024-01-13 09:39:25 +0530

    String Problems

--- RotateStringOptimized.java
+++ RotateStringOptimized.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 /*
     796
@@ -69,3 +70,76 @@ public class RotateStringOptimized {
         
     }
 }
+=======
+
+/*
+    796
+    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+    A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+    For example, if s = "abcde", then it will be "bcdea" after one shift.
+    
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+ */
+
+
+public class RotateStringOptimized {
+    class Solution {
+        public boolean rotateString(String s, String goal) {
+    
+            
+    
+            // find length of start string
+            int n1 = s.length();
+    
+            // find length of goal string
+            int n2 = goal.length();
+    
+            // Step1:
+            // if they are unequal then, start cannot be converted to goal
+            if(n1 != n2)
+            {
+                return false;
+            }
+    
+            // We use StringBuilder as we are modifying the string
+            // String cannot be used as they are immutable.
+            StringBuilder str = new StringBuilder(goal);
+    
+            // Step2:
+            // repeat the goal string.
+            str=str.append(str);
+    
+            // Step3 and Step4:
+            // if start string is present in it then return true.
+            // otherwise return false
+            
+            return (str.indexOf(s)!=-1);   
+    
+            /*
+            The return statement can be expanded as:
+                    if(str.indexOf(s)!=-1)
+                    {
+                         return true;
+                    }
+                    else
+                    {
+                         return false;
+                    }
+            */   
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+>>>>>>> f921878 (String Problems)
