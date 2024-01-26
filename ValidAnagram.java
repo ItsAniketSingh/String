790c855103744b2a93656d95c0978c7d67392035
Aniket Singh <anikets101@gmail.com>
2024-01-26 10:49:42 +0530

    String Problems

---
+++ ValidAnagram.java
@@ -0,0 +1,41 @@
+/*
+    242. Valid Anagram
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+    typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ */
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        return Arrays.equals(ch1, ch2);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
