038d3112c0b2031880d2a8bae076e2933ac2fc3a
Aniket Singh <95512260+ItsAniketSingh@users.noreply.github.com>
2024-02-05 19:44:28 +0530

    Update AnagramOfAString.java

--- AnagramOfAString.java
+++ AnagramOfAString.java
@@ -3,14 +3,11 @@
     Given two strings a and b consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other.
 
     Note:-
-
     If the strings are anagrams you have to return True or else return False
-
     |s| represents the length of string s.
 
 
 Example 1:
-
 Input:a = geeksforgeeks, b = forgeeksgeeks
 Output: YES
 Explanation: Both the string have same characters with
